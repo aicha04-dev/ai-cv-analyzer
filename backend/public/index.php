a815ded8698ad7b87d8dac726b9f1a0f2d18a70e
aicha04-dev <118101565+aicha04-dev@users.noreply.github.com>
2026-09-16 17:37:26 +0100

    Fix Symfony production runtime environment

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,19 +2,8 @@
 
 use App\Kernel;
 
-if (!isset($_SERVER['APP_ENV'])) {
-    $_SERVER['APP_ENV'] = 'prod';
-}
-
-if (!isset($_SERVER['APP_DEBUG'])) {
-    $_SERVER['APP_DEBUG'] = '0';
-}
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
-    return new Kernel(
-        $context['APP_ENV'],
-        (bool) $context['APP_DEBUG']
-    );
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

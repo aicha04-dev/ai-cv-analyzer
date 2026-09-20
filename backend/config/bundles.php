feffeb291a948cf6c3f11af82f64fa89308b49d9
aicha04-dev <118101565+aicha04-dev@users.noreply.github.com>
2026-09-20 18:41:55 +0100

    Remove unused Google authentication

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
